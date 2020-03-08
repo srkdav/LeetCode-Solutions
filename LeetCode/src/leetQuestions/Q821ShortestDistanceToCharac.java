2dde97bd31a91c4c5b21d24d7f029c70970897a8
you@example.com <you@example.com>
2020-03-08 18:29:08 +0530

    LeetCode Sols of March

--- LeetCode/src/leetQuestions/Q821ShortestDistanceToCharac.java
+++ LeetCode/src/leetQuestions/Q821ShortestDistanceToCharac.java
@@ -1,10 +1,71 @@
 package leetQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Q821ShortestDistanceToCharac {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		StringBuilder str = new StringBuilder("loveleetcode");
+		String str = " ";
+		char c ='e';
+		int count=0;
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==c) {
+				count++;
+			}
+		}
+		int[] a = new int[count]; //  1 2 7
+		int j=0;
+		for(int i=0;i<str.length();i++) {
+			if(str.charAt(i)==c) {
+				a[j]=i;
+				j++;
+			}
+		}
+		int[] b = new int[str.length()];
+		for(int i=0;i<b.length;i++) {
+			int min=Integer.MAX_VALUE;
+			if(str.charAt(i)!=c) {
+				for(int k=0;k<a.length;k++) {
+					if(Math.abs(a[k]-i)<min) {
+						min=Math.abs(a[k]-i);
+					}
+				}
+				b[i]=min;
+			}
+		}
+		for(int i=0;i<b.length;i++) {
+			System.out.print(b[i]+" ");
+		}
 	}
 
 }
+
+
+//int[] a = new int[4]; //  1 2 7
+//List<Integer> list = new ArrayList<Integer>();
+//int j=0;
+//for(int i=0;i<str.length();i++) {
+//	if(str.charAt(i)=='e') {
+//		a[j]=i;
+//		j++;
+//	}
+//}
+//int[] b = new int[str.length()];
+//for(int i=0;i<b.length;i++) {
+//	int min=Integer.MAX_VALUE;
+//	if(str.charAt(i)!='e') {
+//		for(int k=0;k<a.length;k++) {
+//			if(Math.abs(a[k]-i)<min) {
+//				min=Math.abs(a[k]-i);
+//			}
+//		}
+//		b[i]=min;
+//	}
+//}
+//for(int i=0;i<b.length;i++) {
+//	System.out.print(b[i]+" ");
+//}
+//}
+
