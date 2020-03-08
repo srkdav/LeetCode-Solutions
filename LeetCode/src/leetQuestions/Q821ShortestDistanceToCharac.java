eb4643a34247d02e4d739db3b6bdb97c65c56a61
you@example.com <you@example.com>
2020-03-08 22:33:37 +0530

    LeetCode Sols of March

--- LeetCode/src/leetQuestions/Q821ShortestDistanceToCharac.java
+++ LeetCode/src/leetQuestions/Q821ShortestDistanceToCharac.java
@@ -6,8 +6,7 @@ import java.util.List;
 public class Q821ShortestDistanceToCharac {
 
 	public static void main(String[] args) {
-//		StringBuilder str = new StringBuilder("loveleetcode");
-		String str = " ";
+		String str = " loveleetcode";
 		char c ='e';
 		int count=0;
 		for(int i=0;i<str.length();i++) {
@@ -41,31 +40,4 @@ public class Q821ShortestDistanceToCharac {
 	}
 
 }
-
-
-//int[] a = new int[4]; //  1 2 7
-//List<Integer> list = new ArrayList<Integer>();
-//int j=0;
-//for(int i=0;i<str.length();i++) {
-//	if(str.charAt(i)=='e') {
-//		a[j]=i;
-//		j++;
-//	}
-//}
-//int[] b = new int[str.length()];
-//for(int i=0;i<b.length;i++) {
-//	int min=Integer.MAX_VALUE;
-//	if(str.charAt(i)!='e') {
-//		for(int k=0;k<a.length;k++) {
-//			if(Math.abs(a[k]-i)<min) {
-//				min=Math.abs(a[k]-i);
-//			}
-//		}
-//		b[i]=min;
-//	}
-//}
-//for(int i=0;i<b.length;i++) {
-//	System.out.print(b[i]+" ");
-//}
-//}
 
