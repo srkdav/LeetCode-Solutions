67e8146800d5f4345f4569d34cc21379b718739e
you@example.com <you@example.com>
2020-03-10 06:39:45 +0530

    LeetCode sols of March

---
+++ LeetCode/src/leetQuestions/Q766ToeplizMatrix.java
@@ -0,0 +1,34 @@
+package leetQuestions;
+
+public class Q766ToeplizMatrix {
+
+	public static void main(String[] args) {
+
+		int[][] a = new int[][] {{1,2,3,4},
+								{5,1,2,3},
+								{9,5,1,2}};
+								
+		int r=a.length;//3
+		int c =a[0].length;//4
+		for(int row=0;row<a.length;row++) {
+			if(row+1<a.length&&a[row][row]==a[row+1][row+1]) {
+				System.out.println("same");
+			}
+		}
+		for(int row=0;row<a.length;row++) {
+			int x=0,y=0;
+			if(a[row][row]==a[row+1][row+1]) {
+				System.out.println("same");
+			}
+		}
+		
+	}
+
+}
+
+//for(int row=0;row<a.length;row++) {
+//	for(int col=0;col<a[row].length;col++) {
+//		System.out.print(a[row][col]);
+//	}
+//	System.out.println();
+//}
