cf15e96a461dd654c353569cc2b55fa6fc5162f7
srkdav <srkdav1994@gmail.com>
2020-03-17 20:08:21 +0530

    Leetcode sols of March

---
+++ LeetCode/src/leetQuestions/Q389FindtheDifference.java
@@ -0,0 +1,53 @@
+package leetQuestions;
+
+import java.util.HashMap;
+
+public class Q389FindtheDifference {
+
+	public static void main(String[] args) {
+
+		 String a="ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsithaqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvopfmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbwhyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuotrrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmfwxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvzsmgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlcaiwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoexmnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor";
+		 String b="qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoygikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxbzskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhveattfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslzwfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllrsbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgveycxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqpqbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbkvpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj";
+//		String a = "aa";
+//		String b = "aa";
+
+		HashMap<Character, Integer> hm1 = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> hm2 = new HashMap<Character, Integer>();
+		for (int i = 0; i < a.length(); i++) {
+			if (hm1.containsKey(a.charAt(i))) {
+				int count = hm1.get(a.charAt(i));
+				count++;
+				hm1.replace(a.charAt(i), count);
+			} else {
+				hm1.put(a.charAt(i), 1);
+			}
+		}
+		for (int i = 0; i < b.length(); i++) {
+			if (hm2.containsKey(b.charAt(i))) {
+				int count = hm2.get(b.charAt(i));
+				count++;
+				hm2.replace(b.charAt(i), count);
+			} else {
+				hm2.put(b.charAt(i), 1);
+			}
+		}
+		for (Character key : hm2.keySet()) {
+			if (hm1.containsKey(key)) {
+				if (hm1.get(key) == hm2.get(key)) {
+					hm2.replace(key, 0);
+				} else {
+					int count = hm2.get(key);
+					count--;
+					hm2.replace(key, count);
+				}
+			}
+		}
+		System.out.println(hm2);
+		for(Character key:hm2.keySet()) {
+			if(hm2.get(key)>0) {
+				System.out.println(key);
+			}
+		}
+	}
+
+}
